3ea7de4df2dcbc628c84157b66c68b15a56cdd24
Rodrigo Balazs <rodrigobalazs@gmail.com>
2025-03-28 22:30:57 -0300

    updates javadoc

--- src/main/java/com/rbalazs/socialapi/model/Post.java
+++ src/main/java/com/rbalazs/socialapi/model/Post.java
@@ -24,9 +24,6 @@ public class Post {
 
     /**
      * Creates a new Post.
-     *
-     * @param theTitle the post title
-     * @param theContent the post content
      */
     public Post(final String theTitle, final String theContent) {
         Validate.notEmpty(theTitle, "The post title cannot be null nor empty");
